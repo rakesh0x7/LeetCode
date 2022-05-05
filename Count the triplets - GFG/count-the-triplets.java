ff3959b36856b2280d0f39fa9b5bfa8fb1834b15
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2022-05-05 07:50:19 +0530

    Added solution - LeetHub

---
+++ Count the triplets - GFG/count-the-triplets.java
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            int n = Integer.parseInt(read.readLine());
+            String input[] = read.readLine().split(" ");
+            
+            int arr[] = new int[n];
+            for(int i = 0;i<n;i++){
+                arr[i] = Integer.parseInt(input[i]);
+            }
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countTriplet(arr, n));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int countTriplet(int arr[], int n) {
+        // code here
+        HashSet<Integer> a = new HashSet<>();
+        for(int i:arr) a.add(i);
+        int cnt=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                int sum = arr[i]+arr[j];
+                if(a.contains(sum)) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
